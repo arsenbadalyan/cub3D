25d2ec2c54bdbd70faaba13b030f70f3fe81a686
Arman <armanmartirosyan81@gmail.com>
2023-07-08 10:22:09 +0400

    sprite working

--- src/game/main.c
+++ src/game/main.c
@@ -6,7 +6,7 @@
 /*   By: armartir <armartir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 10:37:49 by armartir          #+#    #+#             */
-/*   Updated: 2023/07/01 17:37:58 by armartir         ###   ########.fr       */
+/*   Updated: 2023/07/08 10:20:30 by armartir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,52 +22,36 @@ void	my_sprite(t_mlx *mlx)
 
 	g = mlx->game;
 
-	// printf("%f\n%f\n",g->player.pos.x,g->player.pos.y);
+    double spriteX = 1 - g->player.pos.x + 0.2;
+    double spriteY = 7 - g->player.pos.y + 0.2;
 
-	double spriteX = 1 - g->player.pos.x;
-	double spriteY = 7 - g->player.pos.y;
-
-	// double invDet = 1.0 / (g->point.y * g->player.dir.x - g->player.dir.y * g->point.x);
 	double invDet = 1.0 / (g->point.y * g->player.dir.x - g->player.dir.y * g->point.x);
-    
-    // double transformX = invDet * (g->player.dir.x * spriteX - g->player.dir.y * spriteY);
+
     double transformX = invDet * (-g->player.dir.y * spriteX + g->player.dir.x * spriteY);
-	double transformY = invDet * (-g->point.x * spriteY + g->point.y * spriteX) - 1;
-
-	// double	invDet = 1.0 / (g->point.y * g->player.dir.y - g->player.dir.x * g->point.x);
-
-	// double	transformX = invDet * (g->player.dir.y * spriteX - g->player.dir.x * spriteY);
-	// double	transformY = invDet * (-g->point.x * spriteX + g->point.y * spriteY);
-
-    int vMoveScreen = (int)(vMove / transformY);
+	double transformY = invDet * (-g->point.x * spriteY + g->point.y * spriteX);
 
     int spriteScreenX = (int)((W_WIDTH / 2) * (1 + transformX / transformY));
-    
-	int spriteHeight = abs((int)(W_HEIGHT / (transformY)));
-	// int spriteHeight = abs((int)(W_HEIGHT / (transformY))) / vDiv;
+
+    //calculate height of the sprite on screen
+    int spriteHeight = abs((int)(W_HEIGHT / (transformY))); //using 'transformY' instead of the real distance prevents fisheye
+    //calculate lowest and highest pixel to fill in current stripe
 
     int drawStartY = -spriteHeight / 2 + W_HEIGHT / 2;
-    // int drawStartY = -spriteHeight / 2 + W_HEIGHT / 2 + vMoveScreen;
     if(drawStartY < 0) drawStartY = 0;
     int drawEndY = spriteHeight / 2 + W_HEIGHT / 2;
-    // int drawEndY = spriteHeight / 2 + W_HEIGHT / 2 + vMoveScreen;
     if(drawEndY >= W_HEIGHT) drawEndY = W_HEIGHT - 1;
-
-
-    int spriteWidth = abs((int) (W_HEIGHT / (transformY)));
-    // int spriteWidth = abs((int) (W_HEIGHT / (transformY))) / uDiv;
-
-	// printf ("W = %d\nH = %d\n", spriteWidth, spriteHeight);
-
+    //calculate width of the sprite
+    int spriteWidth = abs( (int) (W_HEIGHT / (transformY)));
     int drawStartX = -spriteWidth / 2 + spriteScreenX;
     if(drawStartX < 0) drawStartX = 0;
     int drawEndX = spriteWidth / 2 + spriteScreenX;
     if(drawEndX >= W_WIDTH) drawEndX = W_WIDTH - 1;
 
+	
 	for(int stripe = drawStartX; stripe < drawEndX; stripe++)
     {
       int texX = (int)(256 * (stripe - (-spriteWidth / 2 + spriteScreenX)) * 64 / spriteWidth) / 256;
-      if(transformY > 0 && stripe > 0 && stripe < W_WIDTH )
+      if(transformY > 0 && stripe > 0 && stripe < W_WIDTH && transformY < mlx->zbuffer[stripe])
       for(int y = drawStartY; y < drawEndY; y++)
       {
         int d = (y) * 256 - W_HEIGHT * 128 + spriteHeight * 128;
@@ -104,7 +88,6 @@ void	game_loop(t_mlx *mlx, t_game *game)
 	get_textures(mlx, game, &game->sp.texture, "./sprites/Sprites/barrel.xpm");
 	mlx_loop_hook(mlx->mlx, &create_walls, mlx);
 
-	my_sprite(mlx);
 	
 	mlx_hook(mlx->win, 2, 1L << 0, &keys, mlx);
 	mlx_hook(mlx->win, 6, 1L << 0, &mouse, mlx);
@@ -125,31 +108,3 @@ int	main(int argc, char **argv)
 	game_loop(mlx, game);
 	return (EXIT_SUCCESS);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//	int i = 0;
-// 	int j = 0;
-// 	// puts("ALO");
-// 	while (g->map[i])
-// 	{
-// 		j = 0;	
-// 		while (g->map[i][j])
-// 		{
-// 			// write (1, &g->map[i][j], 1);
-// 			if (g->map[i][j] == '2')
-// 				printf ("i = %d j = %d\n", i,j);
-// 			j++;
-// 		}
-// 		// write(1, "\n",1 );
-// 		i++;
-// 	}
